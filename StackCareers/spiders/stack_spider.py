7d2453e532d03e64fcb587185c0443d125a4ea40
Jon <kingdavies@gmail.com>
2014-08-01 17:17:45 +0100

    Simplify

--- StackCareers/spiders/stack_spider.py
+++ StackCareers/spiders/stack_spider.py
@@ -12,16 +12,13 @@ class StackSpider(CrawlSpider):
     ]
 
     rules = (
-        Rule(SgmlLinkExtractor(restrict_xpaths=('//div[@class="pagination"]/a[text()="next"]')), callback='parse_item', follow=True),
+        Rule(SgmlLinkExtractor(restrict_xpaths=('//div[@class="pagination"]/a[text()="next"]')), callback='parse_start_url', follow=True),
     )
 
-    def parse_item(self, response):
+    def parse_start_url(self, response):
         for sel in response.xpath('//div[@data-jobid]'):
             item = Job()
             item['title'] = sel.xpath('h3/a/text()').extract()
             item['company'] = [x.strip() for x in sel.xpath('p/span[@class="employer"]/text()').extract()]
             #item['company'] = map(str.strip, sel.xpath('p/span[@class="employer"]/text()').extract())
             yield item
-
-    def parse_start_url(self, response):
-        return self.parse_item(response)
