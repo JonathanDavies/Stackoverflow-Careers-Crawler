35d4a49ce298f67a3fcfbb20be981e7ea521d233
Jon <kingdavies@gmail.com>
2014-08-01 17:15:35 +0100

    crawl all pages

--- StackCareers/spiders/stack_spider.py
+++ StackCareers/spiders/stack_spider.py
@@ -1,18 +1,27 @@
 import scrapy
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 
 from StackCareers.items import Job
 
-class StackSpider(scrapy.Spider):
+class StackSpider(CrawlSpider):
     name = "stack"
     allowed_domains = ["careers.stackoverflow.com"]
     start_urls = [
         "http://careers.stackoverflow.com/uk/jobs?searchTerm=&location=london",
     ]
 
-    def parse(self, response):
+    rules = (
+        Rule(SgmlLinkExtractor(restrict_xpaths=('//div[@class="pagination"]/a[text()="next"]')), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
         for sel in response.xpath('//div[@data-jobid]'):
             item = Job()
             item['title'] = sel.xpath('h3/a/text()').extract()
             item['company'] = [x.strip() for x in sel.xpath('p/span[@class="employer"]/text()').extract()]
             #item['company'] = map(str.strip, sel.xpath('p/span[@class="employer"]/text()').extract())
             yield item
+
+    def parse_start_url(self, response):
+        return self.parse_item(response)
